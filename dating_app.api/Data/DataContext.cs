dd98ede32ea8f30908bfb902afdbbe0168d646ac
林博斯 <lb0ss-Mac@LINBOSIdeMacBook-Pro.local>
2019-09-08 18:00:57 -0700

    initial commit with .net web api and angular spa setup

---
+++ dating_app.api/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using dating_app.api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dating_app.api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ dating_app.api/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace dating_app.api.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
