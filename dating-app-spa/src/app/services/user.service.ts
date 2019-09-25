a845e16120efd517088e6550ea66819d5a5fff62
林博斯 <lb0ss-Mac@LINBOSIdeMacBook-Pro.local>
2019-09-24 20:33:26 -0700

    implement angular-jwt to allow automatic attachment of the token to the http request and remove the previous way of manually passing in http headers.

--- dating-app-spa/src/app/services/user.service.ts
+++ dating-app-spa/src/app/services/user.service.ts
@@ -1,32 +1,23 @@
-import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { User } from '../_models/user';
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Authorization': 'Bearer ' + localStorage.getItem('token')
-  })
-};
+import { Injectable } from "@angular/core";
+import { environment } from "src/environments/environment";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { User } from "../_models/user";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
+
 export class UserService {
   baseUrl = environment.apiUrl;
 
-constructor(
-  private http: HttpClient
-) { }
+  constructor(private http: HttpClient) {}
 
-getUsers(): Observable<User[]> {
-  return this.http.get<User[]>(this.baseUrl + 'users', httpOptions);
-}
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.baseUrl + "users");
+  }
 
-getUser(id: number): Observable<User> {
-  return this.http.get<User>(this.baseUrl + 'users/' + id, httpOptions);
-}
-
-
+  getUser(id: number): Observable<User> {
+    return this.http.get<User>(this.baseUrl + "users/" + id);
+  }
 }
