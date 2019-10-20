5ad55a9c11486c44174b5eb3537307423e0f815a
林博斯 <lb0ss-Mac@LINBOSIdeMacBook-Pro.local>
2019-10-19 17:29:51 -0700

    create Message model, extend the User model, and add the Message entity and relationships

--- dating_app.api/Data/DataContext.cs
+++ dating_app.api/Data/DataContext.cs
@@ -15,6 +15,7 @@ namespace dating_app.api.Data
 
         public DbSet<Like> Likes { get; set; }
 
+        public DbSet<Message> Messages { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<Like>()
@@ -32,7 +33,19 @@ namespace dating_app.api.Data
                 .WithMany(u => u.Likees)
                 .HasForeignKey(u => u.LikerId)
                 .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Message>()
+                .HasOne(u => u.Sender)
+                .WithMany(m => m.MessagesSent)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Message>()
+                .HasOne(u => u.Recipient)
+                .WithMany(m => m.MessagesReceived)
+                .OnDelete(DeleteBehavior.Restrict);
+
         }
+        
 
     }
 }
--- dating_app.api/Models/User.cs
+++ dating_app.api/Models/User.cs
@@ -39,5 +39,9 @@ namespace dating_app.api.Models
         public ICollection<Like> Likers { get; set; }
 
         public ICollection<Like> Likees { get; set; }
+
+        public ICollection<Message> MessagesSent { get; set; }
+        
+        public ICollection<Message> MessagesReceived { get; set; }
     }
 }
