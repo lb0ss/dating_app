1dc7e743b9423fdac64515c9add6c1b3ecc3d4af
林博斯 <lb0ss-Mac@LINBOSIdeMacBook-Pro.local>
2019-10-24 19:30:44 -0700

    add the functionality to delete message in the api and the spa

--- dating_app.api/Data/DatingRepository.cs
+++ dating_app.api/Data/DatingRepository.cs
@@ -133,13 +133,14 @@ namespace dating_app.api.Data
                 switch (messageParams.MessageContainer)
                 {
                     case "Inbox":
-                        messages = messages.Where(u => u.RecipientId == messageParams.UserId);
+                        messages = messages.Where(u => u.RecipientId == messageParams.UserId && u.RecipientDeleted == false);
                         break;
                     case "Outbox":
-                        messages = messages.Where(u => u.SenderId == messageParams.UserId);
+                        messages = messages.Where(u => u.SenderId == messageParams.UserId && u.SenderDeleted == false);
                         break;
                     default:
-                        messages = messages.Where(u => u.RecipientId == messageParams.UserId && u.IsRead == false);
+                        messages = messages.Where(u => u.RecipientId == messageParams.UserId 
+                        && u.RecipientDeleted == false  && u.IsRead == false);
                         break;
                 }
             
@@ -152,8 +153,11 @@ namespace dating_app.api.Data
             var messages = await _context.Messages
                 .Include(u => u.Sender).ThenInclude(p => p.Photos)
                 .Include(u => u.Recipient).ThenInclude(p => p.Photos)
-                .Where(m => m.RecipientId == userId && m.SenderId == recipientId || 
-                    m.RecipientId == recipientId && m.SenderId == userId)
+                .Where(
+                    m => m.RecipientId == userId && m.RecipientDeleted == false 
+                    && m.SenderId == recipientId 
+                    || m.RecipientId == recipientId && m.SenderId == userId 
+                    && m.SenderDeleted == false)
                 .OrderByDescending(m => m.MessageSent)
                 .ToListAsync();
 
